d698d4ced519fd60c05bc7ebf013fd4b1852b605
Christhian Rezende Vieira <christhian11rv@hotmail.com>
2025-06-18 23:34:34 -0300

    :hammer: refactor: Changes packages/api folder name to packages/shared

--- apps/api/src/links/links.controller.ts
+++ apps/api/src/links/links.controller.ts
@@ -9,8 +9,8 @@ import {
 } from '@nestjs/common';
 import { LinksService } from './links.service';
 
-import { CreateLinkDto } from '@repo/api/links/dto/create-link.dto';
-import { UpdateLinkDto } from '@repo/api/links/dto/update-link.dto';
+import { CreateLinkDto } from '@repo/shared/links/dto/create-link.dto';
+import { UpdateLinkDto } from '@repo/shared/links/dto/update-link.dto';
 
 @Controller('links')
 export class LinksController {
--- apps/api/src/links/links.service.ts
+++ apps/api/src/links/links.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
-import { Link } from '@repo/api/links/entities/link.entity';
+import { Link } from '@repo/shared/links/entities/link.entity';
 
-import { CreateLinkDto } from '@repo/api/links/dto/create-link.dto';
-import { UpdateLinkDto } from '@repo/api/links/dto/update-link.dto';
+import { CreateLinkDto } from '@repo/shared/links/dto/create-link.dto';
+import { UpdateLinkDto } from '@repo/shared/links/dto/update-link.dto';
 
 @Injectable()
 export class LinksService {
